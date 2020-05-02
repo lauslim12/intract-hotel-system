07e76bb8e5f98b668081941b7a14f6d2c3c57a19
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-02 17:26:19 +0700

    Used CodeIgniter's Query Builder for security and readability.

--- application/models/Hotel_model.php
+++ application/models/Hotel_model.php
@@ -13,8 +13,18 @@ class Hotel_model extends CI_Model {
   }
 
   public function getHotel($id) {
-    $query = $this->db->query("SELECT * FROM hotels WHERE id = '$id' LIMIT 1");
-    return $query->result_array();
+    $this->db->select('*');
+    $this->db->from('hotels');
+    $this->db->join('rooms', 'hotels.id = rooms.hotel_id');
+    $this->db->join('hotel_features', 'hotels.id = hotel_features.hotel_id');
+    $this->db->where('hotels.id', $id);
+    $query = $this->db->get()->result_array();
+
+    $this->db->from('hotel_features');
+    $this->db->where('hotel_id', $id);
+    $query['count_feature'] = $this->db->count_all_results();
+
+    return $query;
   }
 
   public function purchaseRoom($id, $rooms, $user_id, $price, $rooms_after_sale) {
