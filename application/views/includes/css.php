9b3495c355ab4a23eda0d1e89eeeebe1340123dd
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-02 17:24:33 +0700

    Applied DRY principle for CSS loading.

---
+++ application/views/includes/css.php
@@ -0,0 +1,3 @@
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo base_url() . "/assets/css/style.css"; ?>">
+<link rel="shortcut icon" type="image/png" href="<?php echo base_url() . "/assets/images/icons/favicon.png"; ?>">
