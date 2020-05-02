5f32de62c41aacb485037648687ec1ef667b507e
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-02 17:27:29 +0700

    Authentication if the user is already logged in.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,7 +18,17 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('User_model');
+	}
+
 	public function index() {
+		if($this->User_model->isLoggedIn()) {
+			redirect('dashboard');
+		}
+		
 		$this->load->view('landing');
 	}
 }
