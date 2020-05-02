6d75a9ce7773a1a91673364c410a5f105ddd66db
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-03 00:02:29 +0700

    Reset the original routes.

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['booking/(:any)'] = 'dashboard';
