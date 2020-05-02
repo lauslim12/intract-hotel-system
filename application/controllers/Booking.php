7bffb387e561901e3ee48d9ab45abcb3c55783bd
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-03 00:03:11 +0700

    Done Controller functions for booking.

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -12,23 +12,55 @@ class Booking extends CI_Controller {
     redirect('dashboard');
   }
 
+  public function showDetail() {
+    $data = call_frontend($this);
+    $data['id'] = $this->uri->segment(3);
+    $data['hotel'] = $this->Hotel_model->getHotel($data['id']);
+    $data['headlines'] = $this->Hotel_model->getHotelHeadlines($data['id']);
+    $this->load->view('booking', $data);
+  }
+
   public function showBooking() {
     $data = call_frontend($this);
     $data['id'] = $this->uri->segment(3);
     $data['hotel'] = $this->Hotel_model->getHotel($data['id']);
-    $this->load->view('booking', $data);
+    $data['rooms'] = $this->Hotel_model->getHotelRooms($data['id']);
+    $data['headlines'] = $this->Hotel_model->getHotelHeadlines($data['id']);
+    $this->load->view('pages/bookingSection', $data);
+  }
+
+  public function confirmBooking() {
+    $id = $this->input->post('hotel_id');
+    $data = call_frontend($this);
+
+    $data['user_orders'] = [
+      'id' => $this->input->post('hotel_id'),
+      'hotel_name' => $this->input->post('hotel_name'),
+      'num_rooms' => $this->input->post('num_rooms'),
+      'duration' => $this->input->post('duration'),
+      'room' => $this->input->post('room')
+    ];
+
+    $row = $this->Hotel_model->getHotelPrice($id, $data['user_orders']['room']);
+    $data['payment_price'] = $row->price * $data['user_orders']['duration'] * $data['user_orders']['num_rooms'];
+    $data['room_id'] = $row->id;
+
+    $this->load->view('pages/bookingConfirm', $data); 
   }
 
   public function orderHotel() {
-    $id = $this->uri->segment(3);
-    $rooms = $this->input->post('room_count');
-    $user_id = $this->session->userdata('user_id');
-    $price = $this->input->post('price');
+    $purchaseData = [
+      'id' => '',
+      'user_id' => $this->session->userdata('user_id'),
+      'hotel_id' => $this->input->post('hotel_id'),
+      'room_id' => $this->input->post('room_id'),
+      'num_rooms' => $this->input->post('num_rooms'),
+      'duration' => $this->input->post('duration'),
+      'price' => $this->input->post('price'),
+      'finished' => FALSE
+    ];
 
-    $price = $price * $rooms;
-    $room_after_sell = $this->input->post('rooms') - $rooms;
-
-    $this->Hotel_model->purchaseRoom($id, $rooms, $user_id, $price, $room_after_sell);
+    $this->Hotel_model->purchaseRoom($purchaseData);
   }
 
 }
