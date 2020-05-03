74e03a0d2bb7e159f814c2b82145398ef1514049
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-03 12:15:57 +0700

    Imported old Intract's sidebar.

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -3,7 +3,7 @@
 
     <?php
       // Change this after hosting.
-      if(!strpos($_SERVER['REQUEST_URI'], "search") && !strpos($_SERVER['REQUEST_URI'], $user_logged_in)) {
+      if(!strpos($_SERVER['REQUEST_URI'], "search") && !strpos($_SERVER['REQUEST_URI'], $this->session->userdata('username'))) {
         echo '<li class="side-nav__item side-nav__item--active">';
       }
       else {
@@ -12,23 +12,23 @@
     ?>
       <a href="index.php" class="side-nav__link">
         <svg class="side-nav__icon">
-          <use xlink:href="assets/images/svg/sprite.svg#icon-home"></use>
+          <use xlink:href="<?php echo base_url() . "/assets/images/svg/sprite.svg#icon-home"; ?>"></use>
         </svg>
-        <span>Hotel</span>
+        <span>Hotels</span>
       </a>
     </li>
 
     <?php
-      if(strpos($_SERVER['REQUEST_URI'], $user_logged_in)) {
+      if(strpos($_SERVER['REQUEST_URI'], $this->session->userdata('username'))) {
         echo '<li class="side-nav__item side-nav__item--active">';
       }
       else {
         echo '<li class="side-nav__item side-nav__item">';
       }
     ?>
-      <a href="<?php echo $user_logged_in; ?>" class="side-nav__link">
+      <a href="<?php echo $this->session->userdata('username'); ?>" class="side-nav__link">
         <svg class="side-nav__icon">
-          <use xlink:href="assets/images/svg/sprite.svg#icon-aircraft-take-off"></use>
+          <use xlink:href="<?php echo base_url() . "/assets/images/svg/sprite.svg#icon-aircraft-take-off"; ?>"></use>
         </svg>
         <span>My Profile</span>
       </a>
@@ -44,7 +44,7 @@
     ?>
       <a href="#" class="side-nav__link side-nav__link--results">
         <svg class="side-nav__icon">
-          <use xlink:href="assets/images/svg/sprite.svg#icon-magnifying-glass"></use>
+          <use xlink:href="<?php echo base_url() . "/assets/images/svg/sprite.svg#icon-magnifying-glass"; ?>"></use>
         </svg>
         <span>Search Results</span>
       </a>
@@ -53,6 +53,6 @@
   </ul>
 
   <div class="legal">
-    &copy; 2020 by Intract, Bordeaux, Trillo. All Rights Reserved.
+    &copy; 2020 by Intractive, and Trillo. All Rights Reserved.
   </div>
 </nav>
