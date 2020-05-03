53ca05e98b306d5229423d600c79ccda22ee6cff
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-03 17:49:58 +0700

    Hidden the '.php' extension and updated the config file.

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/Intractive/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
