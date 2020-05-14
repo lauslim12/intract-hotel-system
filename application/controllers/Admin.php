2b8f3dbaf6084959be46311829e89d24c12fb33c
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-14 10:55:21 +0700

    Added admin authentication using session.

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,8 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
+  public function __construct() {
+    parent::__construct();
+    $this->load->model('User_model');
+  }
+
   public function index() {
-    $this->load->view('admin');
+    if($this->User_model->isAdmin() == 1) {
+      $this->load->view('admin');
+    }
+    else {
+      redirect('dashboard');
+    }
   }
 
 }
