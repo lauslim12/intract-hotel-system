a78f7c460c3ab39c04abad66b68dd8919dd50185
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-16 18:43:18 +0700

    Added API support for React.js.

---
+++ application/controllers/Api.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class Api extends RestController {
+
+  public function __construct() {
+    parent::__construct();
+    $this->load->model('User_model');
+  }
+
+  public function logged_user_get() {
+    $user = $this->User_model->getUserData($this->session->userdata('username'));
+
+    if(!empty($user)) {
+      $this->response($user, 200);
+    }
+    else {
+      $this->response([
+        'status' => false,
+        'message' => 'No data!'
+      ], 404);
+    }
+  }
+
+}
+
+
