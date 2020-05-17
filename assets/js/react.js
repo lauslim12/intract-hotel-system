ce7f3aefa61b3fcee31d87dccd330945cc0bff9d
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-17 23:25:54 +0700

    Removed redundant components and fixed routing links.

--- assets/js/react.js
+++ assets/js/react.js
@@ -8,6 +8,10 @@ const BASEURL = "http://localhost/Intractive";
 const APIURL = `${BASEURL}/api/logged_user`;
 const LOGOIMG = `${BASEURL}/assets/images/icons/logo.png`;
 const ADMINURL = `${BASEURL}/admin/`;
+const LOGOUTURL = `${BASEURL}/dashboard/logout`;
+const PROFILEURL = `${BASEURL}/profile/view`;
+const HOTELURL = `${ADMINURL}showData`;
+const ORDERURL = `${ADMINURL}showOrders`;
 
 const borderColor = {
   borderColor: '#3f51b5'
@@ -16,56 +20,6 @@ const borderColor = {
 const imageMaxWidth = {
   maxWidth: '60px'
 };
-
-/*
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render() {
-    return (
-      <span>This is {this.state.date.toLocaleTimeString()}!</span>
-    )
-  }
-}
-
-class Navigation extends React.Component {
-  render() {
-    return (
-      <nav classNameName="navbar navbar-expand-lg navbar-light bg-light">
-        <a classNameName="navbar-brand" href={`${BASEURL}`}>Navbar</a>
-        <button classNameName="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span classNameName="navbar-toggler-icon"></span>
-        </button>
-        <div classNameName="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div classNameName="navbar-nav">
-            <a classNameName="nav-item nav-link active" href="#">Home <span classNameName="sr-only">(current)</span></a>
-            <a classNameName="nav-item nav-link" href="#">Features</a>
-            <a classNameName="nav-item nav-link" href="#">Pricing</a>
-            <a classNameName="nav-item nav-link" href="#"><Clock/></a>
-          </div>
-        </div>
-      </nav>
-    )
-  }
-}
-*/
 
 class AlertsCenter extends React.Component {
   render() {
@@ -118,32 +72,6 @@ class AlertsCenter extends React.Component {
         </div>
       </li>
     ) 
-  }
-}
-
-class ProfileDropdown extends React.Component {
-  render() {
-    return (
-      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-        <a class="dropdown-item" href="#">
-          <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-          Profile
-        </a>
-        <a class="dropdown-item" href="#">
-          <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-          Settings
-        </a>
-        <a class="dropdown-item" href="#">
-          <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-          Activity Log
-        </a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="login.html">
-          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-          Logout
-        </a>
-      </div>
-    )
   }
 }
 
@@ -201,7 +129,6 @@ class Header extends React.Component {
                 </form>
               </div>
             </li>
-            <AlertsCenter/>
             <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="https://github.com/lauslim12/intract-hotel-system" id="searchDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
@@ -213,9 +140,19 @@ class Header extends React.Component {
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
                 <img class="img-profile rounded-circle" src={BASEURL + this.state.data.profile_pic} style={imageMaxWidth}></img>
-                <span class="ml-2 d-none d-lg-inline text-white small">{this.state.data.first_name + ' ' + this.state.data.last_name}</span>
+                <span class="ml-2 d-none d-lg-inline text-white small">{this.state.data.username}</span>
               </a>
-            <ProfileDropdown/>
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href={PROFILEURL + '/' + this.state.data.username}>
+                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Profile
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href={LOGOUTURL}>
+                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Logout
+                </a>
+              </div>
             </li>
           </ul>
       </nav>
@@ -280,8 +217,8 @@ class Sidebar extends React.Component {
         <div id="collapseTable" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Tables</h6>
-            <a class="collapse-item" href={BASEURL + '/admin/showData'}>All Hotels</a>
-            <a class="collapse-item" href="datatables.html">All Rooms</a>
+            <a class="collapse-item" href={HOTELURL}>All Hotels</a>
+            <a class="collapse-item" href={ORDERURL}>All Orders</a>
           </div>
         </div>
       </li>
