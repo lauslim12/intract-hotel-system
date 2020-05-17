179fa7edd4f3844e69a79308f963e5d6b962b39a
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-17 12:55:11 +0700

    Added several new methods to display admin panel views.

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -13,7 +13,8 @@ class Admin extends CI_Controller {
   public function index() 
   {
     if($this->User_model->isAdmin() == 1) {
-      $this->load->view('admin');
+      $data = call_frontend_admin($this);
+      $this->load->view('admin', $data);
     }
     else {
       redirect('dashboard');
@@ -23,12 +24,33 @@ class Admin extends CI_Controller {
   public function showData() 
   {
     if($this->User_model->isAdmin() == 1) {
+      $data = call_frontend_admin($this);
       $data['user'] = $this->User_model->getUserData($this->session->userdata('username'));
       $data['hotels'] = $this->Hotel_model->getHotels();
-      $data['header'] = $this->load->view('templates/admin/header.php', NULL, TRUE);
-      $data['sidebar'] = $this->load->view('templates/admin/sidebar.php',  NULL, TRUE);
-      $data['footer'] = $this->load->view('templates/admin/footer.php',  NULL, TRUE);
       $this->load->view('pages/admin/showData', $data);
+    }
+    else {
+      redirect('dashboard');
+    }
+  }
+
+  public function showOrders()
+  {
+    if($this->User_model->isAdmin() == 1) {
+      $data = call_frontend_admin($this);
+      $data['orders'] = $this->Hotel_model->getOrders();
+      $this->load->view('pages/admin/showOrders', $data);
+    }
+    else {
+      redirect('dashboard');
+    }
+  }
+
+  public function showNewHotel()
+  {
+    if($this->User_model->isAdmin() == 1) {
+      $data = call_frontend_admin($this);
+      $this->load->view('pages/admin/showNewHotel', $data);
     }
     else {
       redirect('dashboard');
