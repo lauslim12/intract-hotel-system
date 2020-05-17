e668c1e4088e095878755f18179d8c929ad1796a
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-17 12:54:35 +0700

    Applied DRY principle to load CSS and JS in the admin panel.

--- application/helpers/frontend_helper.php
+++ application/helpers/frontend_helper.php
@@ -8,4 +8,13 @@
     $data['css'] = $object->load->view('includes/css.php', NULL, TRUE);
     return $data;
   }
+
+  function call_frontend_admin($object) {
+    $data['header'] = $object->load->view('templates/admin/header.php', NULL, TRUE);
+    $data['sidebar'] = $object->load->view('templates/admin/sidebar.php',  NULL, TRUE);
+    $data['footer'] = $object->load->view('templates/admin/footer.php',  NULL, TRUE);
+    $data['css'] = $object->load->view('includes/admin/css.php', NULL, TRUE);
+    $data['js'] = $object->load->view('includes/admin/js.php', NULL, TRUE);
+    return $data;
+  }
 ?>
