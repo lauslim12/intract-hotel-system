5ba71fbf948fbc21e607f676e65dbc7c7d6c212c
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-17 12:53:10 +0700

    Modularized the Admin Panel's CSS and JS.

---
+++ application/views/includes/admin/css.php
@@ -0,0 +1,5 @@
+<link href="<?php echo base_url() . "assets/images/icons/favicon.png"; ?>" rel="shortcut icon">
+<link href="<?php echo base_url() . "assets/vendors/fontawesome-free/css/all.min.css"; ?>" rel="stylesheet" type="text/css">
+<link href="<?php echo base_url() . "assets/vendors/bootstrap/css/bootstrap.min.css"; ?>" rel="stylesheet" type="text/css">
+<link href="<?php echo base_url() . "assets/vendors/datatables/dataTables.bootstrap4.min.css"; ?>" rel="stylesheet" type="text/css">
+<link href="<?php echo base_url() . "assets/css/ruang-admin.css"; ?>" rel="stylesheet">
