ec1e188bc846ec7bb69894fbe5592d1629c737f6
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-19 16:56:19 +0700

    Added a small jQuery script to alert a message telling users not to click on the 'Search Results' bar before searching a hotel.

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -54,7 +54,7 @@
       if($this->session->userdata('privilege_level') == 1) {
         echo '<li class="side-nav__item side-nav__item">';
     ?>
-    <a href="<?php echo site_url() . "admin"; ?> " class="side-nav__link side-nav__link--results">
+      <a href="<?php echo site_url() . "admin"; ?> " class="side-nav__link side-nav__link--results">
         <svg class="side-nav__icon">
           <use xlink:href="<?php echo base_url() . "/assets/images/svg/sprite.svg#icon-star"; ?>"></use>
         </svg>
@@ -72,3 +72,10 @@
     &copy; 2020 by Intractive, and Trillo. All Rights Reserved.
   </div>
 </nav>
+
+<!-- Shame. Remove later. It should be in intractive.js. -->
+<script>
+  document.querySelector('.side-nav__link--results').addEventListener('click', function() {
+    alert('Please search in the search box before accessing this page!');
+  });
+</script>
