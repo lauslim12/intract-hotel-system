499f7a74dc9c9953835912470c5bcde34e5287d2
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-19 17:00:31 +0700

    Tidied up the HTML markup.

--- application/views/booking.php
+++ application/views/booking.php
@@ -1,27 +1,21 @@
 <?php
-  defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
+  <meta charset="utf-8">
   <title>Intractive &mdash; Your Hotel</title>
-
   <?php echo $css; ?>
-
 </head>
+
 <body>
   <div class="container">
-    <?php
-      echo $navigation;
-    ?>
-  
+    <?= $navigation; ?>
     <div class="content">
-      <?php
-        echo $sidebar;
-      ?>
-
+      <?= $sidebar; ?>
       <main class="hotel-view">
         <div class="gallery">
           <figure class="gallery__item">
@@ -34,52 +28,45 @@
             <img src="<?php echo base_url() . $headlines[2]['headline_picture']; ?>" alt="Photo 3" class="gallery__photo">
           </figure>
         </div>
-
         <div class="overview">
           <h1 class="overview__heading"><?php echo $hotel[0]['name']; ?></h1>
-          
           <div class="overview__stars">
             <?php
-              for($i = 0; $i < $hotel[0]['star']; $i++) {
-                echo "
+            for ($i = 0; $i < $hotel[0]['star']; $i++) {
+              echo "
                 <svg class='overview__icon-star'>
                   <use xlink:href='" . base_url() . "/assets/images/svg/sprite.svg#icon-star'></use>
                 </svg>
                 ";
-              }
+            }
             ?>
           </div>
-
           <div class="overview__location">
             <svg class="overview__icon-location">
               <use xlink:href="<?php echo base_url() . "/assets/images/svg/sprite.svg#icon-location-pin"; ?>"></use>
             </svg>
             <button class="btn-inline"><?php echo $hotel[0]['location']; ?></button>
           </div>
-
           <div class="overview__rating">
             <div class="overview__rating-average"><?php echo $hotel[0]['rating']; ?></div>
             <div class="overview__rating-count">888 votes</div>
           </div>
-        
         </div>
-        
         <div class="detail">
           <div class="description">
-            
             <p class="paragraph">
               <?php
-                echo $hotel[0]['description'];
+              echo $hotel[0]['description'];
               ?>
             </p>
 
             <ul class='list'>
               <?php
-                for($i = 0; $i < $hotel['count_feature']; $i++) {
-                  echo "
-                    <li class='list__item'>". $hotel[$i]['feature'] . "</li>
+              for ($i = 0; $i < $hotel['count_feature']; $i++) {
+                echo "
+                    <li class='list__item'>" . $hotel[$i]['feature'] . "</li>
                   ";
-                }
+              }
               ?>
             </ul>
 
@@ -97,14 +84,12 @@
             </div>
               -->
           </div>
-          
           <div class="user-reviews">
             <figure class="review">
               <blockquote class="review__text">
-                During a trip to France this summer, I spent my last night at the InterContinental Bordeaux Le Grand Hôtel. 
+                During a trip to France this summer, I spent my last night at the InterContinental Bordeaux Le Grand Hôtel.
                 I’d actually stayed there several years before when it was part of Regent Hotels, but was eager to stay again now that it’s part of IHG.
               </blockquote>
-              
               <figcaption class="review__user">
                 <img src="<?php echo base_url() . "/assets/images/reviewers/reviewer-1.jpg"; ?>" alt="Photo of reviewer 1" class="review__photo">
                 <div class="review__user-box">
@@ -112,15 +97,12 @@
                   <p class="review__user-date">Dec 31st, 2019</p>
                 </div>
                 <div class="review__rating">4.5</div>
-              
               </figcaption>
             </figure>
-            
             <figure class="review">
               <blockquote class="review__text">
                 Our view overlooked one of the hotel’s central courtyards, which was nice and quiet, and the windows actually opened, so we could enjoy some fresh air.
               </blockquote>
-              
               <figcaption class="review__user">
                 <img src="<?php echo base_url() . "/assets/images/reviewers/reviewer-2.jpg"; ?>" alt="Photo of reviewer 2" class="review__photo">
                 <div class="review__user-box">
@@ -128,14 +110,10 @@
                   <p class="review__user-date">Jan 1st, 2020</p>
                 </div>
                 <div class="review__rating">4.75</div>
-              
               </figcaption>
             </figure>
-          
           </div>
         </div>
-        
-
         <!-- 
         <div class="detail">
           <div class="description--posts">
@@ -152,45 +130,36 @@
           </div>
         </div>
         -->
-
         <?php
-          $booking_url = site_url() . "/booking/showBooking/$id";
-          if($rooms != FALSE) {
+        $booking_url = site_url() . "/booking/showBooking/$id";
+        if ($rooms != FALSE) {
         ?>
-
-        <div class="cta">
-          <h2 class="cta__book-now">
-            Good news! Rooms available for you right now!
-          </h2>
-          <button class="btn">
-            <span class="btn__visible">Book now!</span>
-            
-            <span class="btn__invisible">
-              <a href="<?php echo $booking_url; ?>">Only a few left!</a>
-            </span>
-
-          </button>
-        </div>
-
+          <div class="cta">
+            <h2 class="cta__book-now">
+              Good news! Rooms available for you right now!
+            </h2>
+            <button class="btn">
+              <span class="btn__visible">Book now!</span>
+              <span class="btn__invisible">
+                <a href="<?php echo $booking_url; ?>">Only a few left!</a>
+              </span>
+            </button>
+          </div>
         <?php
-          }
-          else {
+        } else {
         ?>
-        
-        <div class="cta">
-          <h2 class="cta__book-now">
-            We're sorry, but all rooms seems to be sold out on this hotel &#128530;
-          </h2>
-        </div>
-
+          <div class="cta">
+            <h2 class="cta__book-now">
+              We're sorry, but all rooms seems to be sold out on this hotel &#128530;
+            </h2>
+          </div>
         <?php
-          }
+        }
         ?>
-
       </main>
     </div>
   </div>
-  
+
   <!--
   <p>booking</p>
   <p>hotel pilihan anda: </p>
@@ -217,4 +186,5 @@
   -->
 
 </body>
+
 </html>
