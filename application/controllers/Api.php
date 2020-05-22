1b08f89076d57d8687774c76b38fbb3894a9bd27
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-22 16:20:16 +0700

    Added methods to communicate with the API.

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -9,23 +9,52 @@ class Api extends RestController {
   {
     parent::__construct();
     $this->load->model('User_model');
+    $this->load->model('Order_model');
+    $this->load->model('Hotel_model');
   }
 
-  public function logged_user_get() 
+  public function respondJSON($data)
   {
-    $user = $this->User_model->getUserData($this->session->userdata('username'));
-
-    if(!empty($user)) {
-      $this->response($user, 200);
+    if(!empty($data)) {
+      $this->response($data, 200);
     }
     else {
       $this->response([
         'status' => false,
         'message' => 'No data!'
       ], 404);
-    }
+    } 
   }
 
+  public function logged_user_get() 
+  {
+    $user = $this->User_model->getUserData($this->session->userdata('username'));
+    $this->respondJSON($user);
+  }
+
+  public function fetch_earnings_get()
+  {
+    $sales = $this->Order_model->getEarnings();
+    $this->respondJSON($sales);
+  }
+
+  public function fetch_sales_get()
+  {
+    $sales = $this->Order_model->getSales();
+    $this->respondJSON($sales);
+  }
+
+  public function fetch_users_get()
+  {
+    $users = $this->User_model->getNumberOfUsers();
+    $this->respondJSON($users);
+  }
+
+  public function fetch_hotels_get()
+  {
+    $hotels = $this->Hotel_model->getNumberOfHotels();
+    $this->respondJSON($hotels);
+  }
 }
 
 
