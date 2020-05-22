4438d4889ea776fa769d7d9e20024410bbe81447
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-22 16:17:49 +0700

    Added methods to get statistics.

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -12,4 +12,53 @@ class Order_model extends CI_Model {
     $this->db->where($notNull);
     return $this->db->count_all_results();
   }
+
+  public function getStatisticsForUserRating()
+  {
+    $query = $this->db->query("SELECT AVG(orders.rating) AS average, 
+                    u.username AS user FROM orders
+                    JOIN users as u on u.id = orders.user_id 
+                    WHERE rating IS NOT NULL;");
+    return $query->result_array();
+  }
+
+  public function getUserRating()
+  {
+    $notNull = "rating IS NOT NULL";
+    $this->db->select_avg('rating');
+    $this->db->from('orders');
+    $this->db->where($notNull);
+    $query = $this->db->get();
+
+    return $query->result_array();
+  }
+
+  public function getHotelName()
+  {
+    $this->db->select('name');
+    $this->db->from('hotels');
+    $query = $this->db->get();
+    
+    return $query->result_array();
+  }
+
+  public function getEarnings()
+  {
+    $this->db->select_sum('price');
+    $this->db->from('orders');
+    $this->db->where('finished', 1);
+    $earnings = $this->db->get()->row()->price;
+    $earnings = str_replace('""', '', $earnings);
+    return json_encode($earnings, JSON_NUMERIC_CHECK);
+  }
+
+  public function getSales()
+  {
+    $notNull = "finished != 0";
+    $this->db->select('id');
+    $this->db->from('orders');
+    $this->db->where($notNull);
+    return json_encode($this->db->count_all_results(), JSON_NUMERIC_CHECK);
+  }
+
 }
