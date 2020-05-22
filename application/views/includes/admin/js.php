91239a74fd9e5609503d7be221fbb95fee2dc16b
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-22 17:26:43 +0700

    Removed vendors JS - I used my own ES6 to utilize Chart.js in my web application.

--- application/views/includes/admin/js.php
+++ application/views/includes/admin/js.php
@@ -3,4 +3,4 @@
 <script src="<?php echo base_url() . "assets/vendors/jquery-easing/jquery.easing.min.js"; ?>"></script>
 <script src="<?php echo base_url() . "assets/js/ruang-admin.js"; ?>"></script>
 <script src="<?php echo base_url() . "assets/vendors/chart.js/Chart.min.js"; ?>"></script>
-<script src="<?php echo base_url() . "assets/js/vendors/demo/chart-area-demo.js"; ?>"></script>
+<script src="<?php echo base_url() . "assets/js/chart.js"; ?>"></script> 
