7963d52ebfe09b127a76f43f75224459fef6258e
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-23 10:30:51 +0700

    Removed 'hard-downloaded' Chart.js dependency.

--- application/views/includes/admin/js.php
+++ application/views/includes/admin/js.php
@@ -2,5 +2,3 @@
 <script src="<?php echo base_url() . "assets/vendors/bootstrap/js/bootstrap.bundle.min.js"; ?>"></script>
 <script src="<?php echo base_url() . "assets/vendors/jquery-easing/jquery.easing.min.js"; ?>"></script>
 <script src="<?php echo base_url() . "assets/js/ruang-admin.js"; ?>"></script>
-<script src="<?php echo base_url() . "assets/vendors/chart.js/Chart.min.js"; ?>"></script>
-<script src="<?php echo base_url() . "assets/js/chart.js"; ?>"></script> 
