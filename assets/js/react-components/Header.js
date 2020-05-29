d06f88182c272de2f5d472e5618b1ba17e2fa085
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-29 18:57:48 +0700

    Removed useless search icon.

--- assets/js/react-components/Header.js
+++ assets/js/react-components/Header.js
@@ -3,9 +3,6 @@ import { StyleSheet, css } from 'aphrodite';
 import { BASEURL, APIURL, LOGOUTURL, PROFILEURL } from '../react-variables/Variables';
 
 const aphroditeStyle = StyleSheet.create({
-  blueBorder: {
-    borderColor: '#3f51b5'
-  },
   imageProfile: {
     maxWidth: '60px',
     height: '2rem',
@@ -49,27 +46,7 @@ class Header extends React.Component {
           </button>
           <ul className="navbar-nav ml-auto">
             <li className="nav-item dropdown no-arrow">
-              <a className="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                <i className="fas fa-search fa-fw"></i>
-              </a>
-              <div className="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                aria-labelledby="searchDropdown">
-                <form className="navbar-search">
-                  <div className="input-group">
-                    <input type="text" className="form-control bg-light border-1 small" placeholder="What do you want to look for?"
-                      aria-label="Search" aria-describedby="basic-addon2" className={css(aphroditeStyle.blueBorder)}/>
-                    <div className="input-group-append">
-                      <button className="btn btn-primary" type="button">
-                        <i className="fas fa-search fa-sm"></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </li>
-            <li className="nav-item dropdown no-arrow">
-            <a className="nav-link dropdown-toggle" href="https://github.com/lauslim12/intract-hotel-system" id="searchDropdown" role="button" data-toggle="dropdown"
+              <a className="nav-link dropdown-toggle" href="https://github.com/lauslim12/intract-hotel-system" id="searchDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
                 <i className="fab fa-fw fa-github"></i>
               </a>
