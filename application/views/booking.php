f14dcadc4bdd6ffb795cc938877afeb376cb681e
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-29 11:36:56 +0700

    Re-added the meta tags for reseponsiveness.

--- application/views/booking.php
+++ application/views/booking.php
@@ -7,6 +7,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Intractive &mdash; Your Hotel</title>
   <?php echo $css; ?>
 </head>
